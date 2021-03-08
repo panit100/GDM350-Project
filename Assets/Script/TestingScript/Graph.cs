4929e25a9df90884a080251ad513dbfcea1b729f
Panit <panit.cuteza@mail.kmutt.ac.th>
2021-03-08 21:29:40 +0700

    -Commit 3/8/21
    TestingGraph
    -Graph.cs

---
+++ Assets/Script/TestingScript/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+    [SerializeField,Range(10,100)]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f /resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
+        
+        points = new Transform[resolution];
+        for(int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i+0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+        }
+
+        
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        for(int i = 0; i< points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
